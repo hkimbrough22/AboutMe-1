e088c7a3745add470089909ebde72fb1fdd772b8
Abdifatah Mohamed <abdimohamed121@gmail.com>
2021-07-14 20:56:48 -0500

    Lab2

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# AboutMe
-Guessing game/questions
+# About Me
+## Guessing game/questions
+
+### *Lab 2:*
+I created a page about me for others to visit. The goal of this project was to create a page that has my Bio, education, experience and goals. I also created a prompt for visitors to interact, where they provide their name and play a guessing game on how well they know me. The answers the user provides is logged in the console and I created a costume greeting that welcomes the user by name. After they play the guessing game, the page provides them with their correct guess score for all 5 questions. It then gives them a final welcome message and gives them access to the page. 
---
+++ js/app.js
@@ -0,0 +1,80 @@
+'use strict';
+
+// this statement asks the visitor for their name using prompt. And the while loop will keep asking for their name until they do so.
+let userName = prompt('Hello dear visitor, whats your name?');
+console.log(userName);
+while(!userName){
+  alert('Please enter your name');
+  userName = prompt('Lets try this again. Whats your name?');
+}
+alert('Hi ' + userName + ' I would like to thank you for visiting my page!');
+
+alert('Let\'s have a bit of fun ' + userName + ' by playing a guessing game! Please answer with y/n!');
+
+// this variable will count the number of questions the user guessed correctly.
+let correctGuesses = 0;
+
+let firstQ = prompt('Did i serve in the Military?');
+let firstFont = firstQ.toLowerCase();
+if(firstFont === 'yes' || firstFont === 'y' || firstFont === 'yea'){
+  alert('That\'s correct! I served in the US Navy for 8 years');
+  correctGuesses++;
+}else if(firstFont === 'no' || firstFont === 'n' || firstFont === 'nah'){
+  alert('Sorry that\'s not correct. I did serve in the US Navy for 8 years');
+}else{
+  alert('Please answer with a yes or no!');
+}
+console.log(userName, 'answered the first question:' , firstFont);
+
+let secondQ = prompt('Did i work in the Aerospace industry?');
+let secondFont = secondQ.toLowerCase();
+if(secondFont === 'yes' || secondFont === 'y' || secondFont === 'yea'){
+  alert('That\'s correct! I worked in the Aerospace industry as a Value Stream Manager.');
+  correctGuesses++;
+}else if(secondFont === 'no' || secondFont === 'n' || secondFont === 'nah'){
+  alert('Sorry that\'s not correct. I did work in the Aerospace industry');
+}else{
+  alert('Please answer with a yes or no!');
+}
+console.log(userName, 'answered the second question:' , secondFont);
+
+
+let thirdQ = prompt('Do I live in Texas?');
+let thirdFont = thirdQ.toLowerCase();
+if(thirdFont === 'no' || thirdFont === 'n' || thirdFont === 'nah'){
+  alert('That\'s correct! I live in Minnesota');
+  correctGuesses++;
+}else if(thirdFont === 'yes' || thirdFont === 'y' || thirdFont === 'yea'){
+  alert('Sorry that\'s not correct. I never lived in Texas');
+}else{
+  alert('Please answer with a yes or no!');
+}
+console.log(userName, 'answered the third question:' , thirdFont);
+
+
+let FourthQ = prompt('Did i graduate from college?');
+let fourthFont = FourthQ.toLowerCase();
+if(fourthFont === 'yes' || fourthFont === 'y' || fourthFont === 'yea'){
+  alert('That\'s correct! I graduated from the University of California Irvine');
+  correctGuesses++;
+}else if(fourthFont === 'no' || fourthFont === 'n' || fourthFont === 'nah'){
+  alert('Sorry that\'s not correct. I graduated from the University of California Irvine');
+}else{
+  alert('Please answer with a yes or no!');
+}
+console.log(userName, 'answered the fourth question:' , fourthFont);
+
+
+let fifthQ = prompt('Have i ever been to Japan?');
+let fifthFont = fifthQ.toLowerCase();
+if(fifthFont === 'yes' || fifthFont === 'y' || fifthFont === 'yea'){
+  alert('That\'s correct! I was stationed in Japan for some time while in the Navy');
+  correctGuesses++;
+}else if(fifthFont === 'no' || fifthFont === 'n' || fifthFont === 'nah'){
+  alert('Sorry that\'s not correct. I was stationed in Japan for sometime');
+}else{
+  alert('Please answer with a yes or no!');
+}
+console.log(userName, 'answered the fifth question:' , fifthFont);
+
+alert('Well done ' + userName + '!. You guessed ' + correctGuesses + '/5 correct. Thank you for playing this guessing game with me. Welcome to my page ' + userName + '!');
